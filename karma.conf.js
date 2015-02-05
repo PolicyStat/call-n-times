1ad0ee0c8f4e01c0c333bbd50eb0ef4bbbc9c742
Shahar Or <mightyiampresence@gmail.com>
2015-02-05 18:40:10 +0200

    Move test file

--- karma.conf.js
+++ karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      "test/**/*spec.js"
+      "spec/spec.js"
     ],
 
 
@@ -36,7 +36,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        "test/**/*spec.js": ["browserify"]
+        "spec/spec.js": ["browserify"]
     },
 
 
