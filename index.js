3f069af82cbb6053d8e1b99a6c6b5253f7ca2c69
Shahar Or <mightyiampresence@gmail.com>
2015-04-27 17:20:33 +0300

    JS Standard Style update

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ var isInteger = require('validate.io-integer')
 
 module.exports = function (func, n, cb) {
   if (arguments.length < 2) {
-    throw 'Expected exactly 2 arguments'
+    throw new Error('Expected exactly 2 arguments')
   }
   if (typeof func !== 'function') {
-    throw 'Expected first argument to be a function'
+    throw new Error('Expected first argument to be a function')
   }
   if (typeof n !== 'number') {
-    throw 'Expected second argument to be a number'
+    throw new Error('Expected second argument to be a number')
   }
   if (n >= 0 !== true) {
-    throw 'Expected second argument to be equal to or greater than 0'
+    throw new Error('Expected second argument to be equal to or greater than 0')
   }
   if (isInteger(n) !== true) {
-    throw 'Expected second argument to be an integer'
+    throw new Error('Expected second argument to be an integer')
   }
 
   var returns = []
--- spec/spec.js
+++ spec/spec.js
@@ -4,52 +4,52 @@
 
 var call = require('..')
 
-describe('argument validation', function() {
-  it('throws if provided no arguments', function() {
-    expect(function() {
+describe('argument validation', function () {
+  it('throws if provided no arguments', function () {
+    expect(function () {
       call()
     }).toThrow()
   })
-  it('throws if provided one argument', function() {
-    expect(function() {
+  it('throws if provided one argument', function () {
+    expect(function () {
       call('foo')
     }).toThrow()
   })
-  it('throws if the first argument is not a function', function() {
-    expect(function() {
+  it('throws if the first argument is not a function', function () {
+    expect(function () {
       call('foo', 2)
     }).toThrow()
   })
-  it('throws if the second argument is not a number', function() {
-    expect(function() {
-      call(function() {}, 'foo')
+  it('throws if the second argument is not a number', function () {
+    expect(function () {
+      call(function () {}, 'foo')
     }).toThrow()
   })
-  it('throws if the second argument is less than zero', function() {
-    expect(function() {
-      call(function() {}, -1)
+  it('throws if the second argument is less than zero', function () {
+    expect(function () {
+      call(function () {}, -1)
     }).toThrow()
   })
 })
 
-describe('the calls', function() {
+describe('the calls', function () {
   var spy = jasmine.createSpy('spy')
 
-  afterEach(function() {
+  afterEach(function () {
     spy.calls.reset()
   })
 
-  it('can get called 0 times', function() {
+  it('can get called 0 times', function () {
     call(spy, 0)
     expect(spy).not.toHaveBeenCalled()
   })
 
-  it('can called 1 time', function() {
+  it('can called 1 time', function () {
     call(spy, 1)
     expect(spy.calls.count()).toEqual(1)
   })
 
-  it('can get called 2 and more times', function() {
+  it('can get called 2 and more times', function () {
     for (var n = 5; n !== 0; n--) {
       call(spy, n)
       expect(spy.calls.count()).toEqual(n)
@@ -58,12 +58,12 @@ describe('the calls', function() {
   })
 })
 
-describe('the return value', function() {
-  it('is in order', function() {
+describe('the return value', function () {
+  it('is in order', function () {
     var n = 100
     var i = 0
     var returns = call(
-      function() {
+      function () {
         var returnVal = i.toString()
         i++
         return returnVal
@@ -77,13 +77,13 @@ describe('the return value', function() {
   })
 })
 
-describe('the callback function', function() {
-  it('is called after all calls', function() {
+describe('the callback function', function () {
+  it('is called after all calls', function () {
     var cbCalled = false
-    var callback = function() {
+    var callback = function () {
       cbCalled = true
     }
-    var func = function() {
+    var func = function () {
       cbCalled = false
     }
     call(func, 2, callback)
