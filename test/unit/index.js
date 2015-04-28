489a71394967abc925e24edf3620cd5934578801
Shahar Or <mightyiampresence@gmail.com>
2015-04-28 17:42:05 +0300

    Way better unit tests

--- test/unit/index.js
+++ test/unit/index.js
@@ -8,72 +8,81 @@ var sinonChai = require('sinon-chai')
 chai.should()
 chai.use(sinonChai)
 
-var call = require('../..')
+var callNTimes = require('../..')
 
-describe('argument validation', function () {
-  it('throws if provided no arguments', function () {
-    call.should.throw
-  })
+describe('call-n-times', function () {
   // https://github.com/eslint/eslint/issues/2402
   /* eslint-disable no-wrap-func */
-  it('throws if provided one argument', function () {
-    (function () {call('foo')}).should.throw
-  })
-  it('throws if the first argument is not a function', function () {
-    (function () {call('foo', 2)}).should.throw
-  })
-  it('throws if the second argument is not a number', function () {
-    (function () {call(function () {}, 'foo')}).should.throw
-  })
-  it('throws if the second argument is less than zero', function () {
-    (function () {call(function () {}, -1)}).should.throw
-  })
-  /* eslint-enable no-wrap-func */
-})
-
-describe('the calls', function () {
-  var spy = sinon.spy()
-
-  afterEach(function () {
-    spy.reset()
-  })
-
-  it('can get called 0 times', function () {
-    call(spy, 0)
-    spy.should.not.have.been.called
-  })
-
-  it('can called 1 time', function () {
-    call(spy, 1)
-    spy.should.have.been.calledOnce
+  context('called with invalid arguments', function () {
+    context('no arguments', function () {
+      it('throws', function () {
+        callNTimes.should.throw()
+      })
+    })
+    context('only one argument', function () {
+      it('throws', function () {
+        (function () {callNTimes('foo')}).should.throw()
+      })
+    })
+    context('first argument not a function', function () {
+      it('throws', function () {
+        (function () {callNTimes('foo', 2)}).should.throw()
+      })
+    })
+    context('second argument not a number', function () {
+      it('throws', function () {
+        (function () {callNTimes(function () {}, 'foo')}).should.throw()
+      })
+    })
+    context('second argument < 0', function () {
+      it('throws', function () {
+        (function () {callNTimes(function () {}, -1)}).should.throw()
+      })
+    })
   })
 
-  it('can get called 2 and more times', function () {
+  context('`func` is a function and `n` is', function () {
     var n = 0
-    while (n !== 5) {
-      call(spy, n)
-      spy.should.have.callCount(n)
-      spy.reset()
+    while (n !== 13) {
+      context(n, function () {
+        // in each iteration, the return value of call-n-times will be
+        // assigned to `returns`
+        var returns
+
+        var o = 9 // counter for function below
+        // this function will be passed to call-n-times as `func` in
+        // each iteration. As a result, the array returned by call-n-times
+        // will be an integer series from an arbitrary 10 to `n` + 10
+        var generateIncrementalReturnValues = function () {
+          o++
+          return o
+        }
+
+        var spy = sinon.spy(generateIncrementalReturnValues)
+
+        it('does not throw', function () {
+          (function () {
+            returns = callNTimes(spy, n)
+          }).should.not.throw()
+        })
+
+        it('calls `func` `' + n + '` times', function () {
+          spy.should.have.callCount(n)
+        })
+
+        it('returns an array of all the return values', function () {
+          returns.should.be.an('array')
+          returns.length.should.equal(n)
+          // the return value of call-n-times is an array, an integer
+          // series from 11 to `n` + 10
+          returns.forEach(function (value, index) {
+            value.should.equal(index + 10)
+          })
+        })
+      })
+
       n++
     }
   })
-})
-
-describe('the return value', function () {
-  it('is in order', function () {
-    var n = 100
-    var i = 0
-    var returns = call(
-      function () {
-        var returnVal = i.toString()
-        i++
-        return returnVal
-      },
-      n
-    )
-    // Reusing `i`
-    for (i = 0; i < n; i++) {
-      returns[i].should.equal(i.toString())
-    }
-  })
+  /* eslint-enable no-wrap-func */
 })
